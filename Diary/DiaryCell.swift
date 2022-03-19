bbc500195991efaef1b955d16fe774f9699e322b
이승후 <ron222@naver.com>
2022-03-20 00:08:29 +0900

    Diary app UI set

---
+++ Diary/DiaryCell.swift
@@ -0,0 +1,12 @@
+//
+//  DiaryCell.swift
+//  Diary
+//
+//  Created by 이승후 on 2022/03/19.
+//
+
+import UIKit
+
+class DiaryCell: UICollectionViewCell {
+    
+}
---
+++ Diary/StarCell.swift
@@ -0,0 +1,12 @@
+//
+//  StarCell.swift
+//  Diary
+//
+//  Created by 이승후 on 2022/03/19.
+//
+
+import UIKit
+
+class StarCell: UICollectionViewCell {
+    
+}
