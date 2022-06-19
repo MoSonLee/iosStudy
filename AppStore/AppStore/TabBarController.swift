f35d719594aa33882ff43cf48696233c2abaa483
이승후 <ron222@naver.com>
2022-06-19 22:11:32 +0900

    Add: AppStore app

---
+++ AppStore/AppStore/TabBarController.swift
@@ -0,0 +1,42 @@
+//
+//  TabBarController.swift
+//  AppStore
+//
+//  Created by 이승후 on 2022/06/19.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+  
+  private lazy var todayViewController: UIViewController =  {
+    let viewController = UIViewController()
+    let tabBarItem = UITabBarItem(
+      title: "Today",
+      image:UIImage(systemName: "mail"),
+      tag: 0
+    )
+    viewController.tabBarItem = tabBarItem
+    
+    return viewController
+  }()
+  
+  private lazy var appViewController: UIViewController =  {
+    let viewController = UIViewController()
+    let tabBarItem = UITabBarItem(
+      title: "App",
+      image:UIImage(systemName: "square.stack.3d.up"),
+      tag: 1
+    )
+    viewController.tabBarItem = tabBarItem
+    
+    return viewController
+  }()
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    viewControllers = [todayViewController, appViewController]
+  }
+}
+
