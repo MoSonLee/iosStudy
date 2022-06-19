4aa20106c73c49990d9e37640047d435798c06c2
이승후 <ron222@naver.com>
2022-06-19 21:32:50 +0900

    Add: AppStore App file

---
+++ AppStoreApp/AppStoreApp/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  AppStoreApp
+//
+//  Created by 이승후 on 2022/06/19.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+  
+  var window: UIWindow?
+  
+  
+  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    guard let windowScene = (scene as? UIWindowScene) else { return }
+    
+    window = UIWindow(windowScene: windowScene)
+    window?.backgroundColor = .systemBackground
+    window?.rootViewController = TabBarController()
+    window?.makeKeyAndVisible()
+  }
+}
+
---
+++ AppStoreApp/AppStoreApp/TabBarController.swift
@@ -0,0 +1,20 @@
+//
+//  TabBarController.swift
+//  AppStoreApp
+//
+//  Created by 이승후 on 2022/06/19.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.backgroundColor = .red
+  }
+
+
+}
+
